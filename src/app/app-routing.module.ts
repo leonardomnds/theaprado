6dcdf8dc97f9a4d162be8ade1fa69ace3b803c48
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2024-05-27 20:50:47 -0300

    chore: implementa ssg

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,6 @@ const routes: Routes = [
 
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(routes)]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })]
 })
 export class AppRoutingModule { }
