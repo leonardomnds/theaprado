615d460b67091188d68b21eff59e5c4cd7baa148
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2024-05-27 16:41:31 -0300

    feat: inicia projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   whatsappUrl: 'https://wa.me/+5545991572139',
   instagramUrl: 'https://www.instagram.com/arquiteturatheaprado',
-  googleMapsUrl: 'https://www.google.com/maps/place/Thea+Prado+Arquitetura/@-24.732808,-53.7548626,16.25z/data=!4m6!3m5!1s0x94f3959a28cfffab:0x61bc763d37872640!8m2!3d-24.7321077!4d-53.7529788!16s%2Fg%2F11c59p_dn6?entry=ttu'
+  googleMapsUrl: 'https://maps.app.goo.gl/kaJmWkjFPhPf347X7'
 };
