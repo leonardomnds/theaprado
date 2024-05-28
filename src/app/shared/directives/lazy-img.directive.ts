c51e8e2c3d01542040e56c7602a26a9fd3c96acb
Leonardo Mendes <leonardo.mendes@insidesistemas.com.br>
2024-05-28 15:36:53 -0300

    feat: add lazy loading to images

--- src/app/shared/directives/lazy-img.directive.ts
+++ src/app/shared/directives/lazy-img.directive.ts
@@ -1,12 +1,29 @@
-import { Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, inject } from '@angular/core';
 
 @Directive({ selector: 'img' })
-export class LazyImgDirective {
-  constructor({ nativeElement }: ElementRef<HTMLImageElement>) {
-    const supports = 'loading' in HTMLImageElement.prototype;
+export class LazyImgDirective implements AfterViewInit {
 
-    if (supports) {
-      nativeElement.setAttribute('loading', 'lazy');
-    }
+  private elementRef: ElementRef<HTMLImageElement> = inject(ElementRef<HTMLImageElement>);
+
+  ngAfterViewInit() {
+    const imgElement = this.elementRef.nativeElement;
+    const originalSrc = imgElement.src;
+
+    imgElement.setAttribute('data-src', originalSrc);
+    imgElement.setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const img = entry.target as HTMLImageElement;
+          const src = img.getAttribute('data-src');
+          if (src) { img.setAttribute('src', src); }
+          observer.unobserve(img);
+        }
+      });
+    });
+
+    observer.observe(imgElement);
   }
+
 }
